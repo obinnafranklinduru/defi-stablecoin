9bcece317ef66bc2464c36a524bc57f32d115c84
obinnafranklinduru <obinnafranklinduru@gmail.com>
2025-03-06 17:28:34 +0100

    feat/Working on the EPDEngine Contract

--- src/EPDEngine.sol
+++ src/EPDEngine.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { ExoPegStablecoin } from "./ExoPegStablecoin.sol";
-
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ExoPegStablecoin} from "./ExoPegStablecoin.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /*
  * @title EPDEngine
@@ -13,34 +13,72 @@ import { ExoPegStablecoin } from "./ExoPegStablecoin.sol";
  * 
  * @dev Inspired by MakerDAO's DSS system, this minimal engine focuses on essential functions to ensure stability and simplicity.
  */
-contract EPDEngine is ReentrancyGuard  {
+contract EPDEngine is ReentrancyGuard {
     error EPDEngine__CollateralAmountMustBeMoreThanZero();
     error EPDEngine__InvalidTokenAddress();
+    error EPDEngine__TransferFailed();
+    error EPDEngine__TokenAddressesAndPriceFeedsLengthMismatch();
 
-    modifier notZero(uint256 _amount){
+    ExoPegStablecoin private immutable EPD;
+
+    mapping(address token => address priceFeed) private priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private collateralDeposited;
+
+    event CollateralDeposited(address indexed user, address indexed token, uint256 amount);
+
+    modifier notZero(uint256 _amount) {
         if (_amount <= 0) revert EPDEngine__CollateralAmountMustBeMoreThanZero();
         _;
     }
 
     modifier isAllowedTokens(address _token) {
-        if (_token != address(0)) revert EPDEngine__InvalidTokenAddress();
+        if (priceFeeds[_token] == address(0)) revert EPDEngine__InvalidTokenAddress();
         _;
+    }
+
+    constructor(address[] memory _tokenAddresses, address[] memory _priceFeedAddresses, address _EPDAddress) {
+        if (_tokenAddresses.length != _priceFeedAddresses.length) {
+            revert EPDEngine__TokenAddressesAndPriceFeedsLengthMismatch();
+        }
+
+        for (uint256 i = 0; i < _tokenAddresses.length; i++) {
+            priceFeeds[_tokenAddresses[i]] = _priceFeedAddresses[i];
+        }
+        EPD = ExoPegStablecoin(_EPDAddress);
     }
 
     function depositCollateralAndMintEPD() external {}
 
-    function depositCollateral() external {}
+    /**
+     * @param _tokenCollateralAddress The address of the token to deposit as collateral
+     * @param _amountCollateral The amount of collateral to deposit
+     */
+    function depositCollateral(address _tokenCollateralAddress, uint256 _amountCollateral)
+        external
+        isAllowedTokens(_tokenCollateralAddress)
+        notZero(_amountCollateral)
+        nonReentrant
+    {
+        collateralDeposited[msg.sender][_tokenCollateralAddress] += _amountCollateral;
+        emit CollateralDeposited(msg.sender, _tokenCollateralAddress, _amountCollateral);
+
+        bool success = IERC20(_tokenCollateralAddress).transferFrom(msg.sender, address(this), _amountCollateral);
+        if(!success) revert EPDEngine__TransferFailed();
+    }
 
     function redeemCollateralForEPD() external {}
 
     function redeemCollateral() external {}
 
-    function mintEPD() external {}
+    /**
+     * @param _amountEPDToMint The amount of EPD tokens to mint
+     * @notice User must have more collateral value than the minimum threshold
+     */
+    function mintEPD(uint256 _amountEPDToMint) external notZero(_amountEPDToMint) {}
 
     function burnEPD() external {}
 
     function liquidate() external {}
 
     function getHealthFactor() external view {}
-
 }
